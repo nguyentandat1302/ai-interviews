bd0d18c44a212e4bab26e0bf33cc0f1c80ba8588
RuanChenDa <21280103e0017@student.tdmu.edu.vn>
2025-09-17 15:51:52 +0700

    setup db firebase

---
+++ src/app/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaWIB51m3m8Sv0u_jtvXEd41c064FhYak",
+  authDomain: "ai-interview-5863c.firebaseapp.com",
+  projectId: "ai-interview-5863c",
+  storageBucket: "ai-interview-5863c.firebasestorage.app",
+  messagingSenderId: "1077521349218",
+  appId: "1:1077521349218:web:535a301576444405adaf3b",
+  measurementId: "G-WEKJFC9Z58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
