57972578a2eca5f9a9f783429c4d75def28bcb63
RuanChenDa <21280103e0017@student.tdmu.edu.vn>
2025-09-18 22:25:09 +0700

    .

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -2,12 +2,11 @@
 
 import * as React from "react"
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
-import theme from "@/theme" // nếu bạn có file theme.ts, không thì xoá
+import theme from "@/theme" 
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider theme={theme}>
-      {/* Cho phép Chakra nhận biết light/dark mode */}
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       {children}
     </ChakraProvider>
